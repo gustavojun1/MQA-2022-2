19d5f0d095ebb1119c77c475f2f4655e63bde5fc
jun <gustavonagatomo@gmail.com>
2022-12-10 11:56:07 -0300

    Finalização do script para o dataset real da atividade 7

--- Atividade 7 (Análise Fatorial)/script_final.R
+++ Atividade 7 (Análise Fatorial)/script_final.R
@@ -16,6 +16,17 @@ dados.rcorr = rcorr(as.matrix(dados))
 View(dados.rcorr$r)
 
 # calculando o coeficiente KMO
+# já dá também o MSA para cada variável (Etapa 4)
 library(psych)
 KMO(dados)
 
+# teste de Bartlett da matriz de correlação (retirando Nota_Credito)
+bartlett.test(list(dados$Idade, dados$Tempo_de_Conta, dados$Salario, dados$Saldo, dados$Numero_Produtos))
+# como p-value >> 0.05, eu NÃO REJEITO h0, ou seja, AF não é uma análise adequada
+
+# determinação do número de fatores
+library(FactoMineR)
+pca = PCA(dados)
+summary(pca, ncp=4)
+library(factoextra)
+fviz_screeplot(pca, ncp = 5)
