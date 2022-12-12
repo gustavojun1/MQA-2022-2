08514dfa3356db4fcda1c6f4a13e0e74d5f1897f
jun <gustavonagatomo@gmail.com>
2022-12-12 11:33:35 -0300

    Finalizando (agora de verdade) o script da atividade 7.

--- Atividade 7 (Análise Fatorial)/script_final.R
+++ Atividade 7 (Análise Fatorial)/script_final.R
@@ -27,6 +27,23 @@ bartlett.test(list(dados$Idade, dados$Tempo_de_Conta, dados$Salario, dados$Saldo
 # determinação do número de fatores
 library(FactoMineR)
 pca = PCA(dados)
-summary(pca, ncp=4)
 library(factoextra)
 fviz_screeplot(pca, ncp = 5)
+
+# rotação via Varimax
+dados.fac = factanal(na.omit(dados), factors = 2, rotation = "varimax")
+dados.fac
+plot(dados.fac$loadings[,1], 
+     dados.fac$loadings[,2],
+     xlab = "Fator 1", 
+     ylab = "Fator 2", 
+     ylim = c(-1,1),
+     xlim = c(-1,1),
+     main = "Rotação Varimax")
+text(dados.fac$loadings[,1]-0.1, 
+     dados.fac$loadings[,2]+0.1,
+     colnames(dados),
+     col="blue",
+     cex = 0.6)
+abline(h = 0, v = 0)
+
